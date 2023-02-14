af0e8ccdaa927008b7346cdfec58ee02d3bcdba3
mohammedimjan <mohammedimjancyb@gmail.com>
2023-02-14 16:49:12 +0530

    product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from "./data";
+
 function App() {
 	return (
 		<div className="App">
@@ -6,7 +8,27 @@ function App() {
 					Ama<span>zona</span>
 				</a>
 			</header>
-			<main>List Products</main>
+			<main>
+				<h1>Featured Products</h1>
+				<div className="products">
+					{data.products.map((product) => (
+						<div className="product" key={product.slug}>
+							<a href={product.slug}>
+								<img src={product.image} alt={product.name} />
+							</a>
+							<div className="product-info">
+								<a href={product.slug}>
+									<p>{product.name}</p>
+								</a>
+								<p>
+									<strong>${product.price}</strong>
+								</p>
+								<button>Add To Cart</button>
+							</div>
+						</div>
+					))}
+				</div>
+			</main>
 		</div>
 	);
 }
