1220e46280f5b42d10842d81941d08cf8d14d6d9
mohammedimjan <mohammedimjancyb@gmail.com>
2023-02-14 08:55:44 +0530

    Create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<a href="/">
+					Ama<span>zona</span>
+				</a>
+			</header>
+			<main>List Products</main>
+		</div>
+	);
+}
+
+export default App;
